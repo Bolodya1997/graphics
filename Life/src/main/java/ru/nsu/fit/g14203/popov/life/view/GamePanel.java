4a5d206c904b3bc2925c15fc97ef11df760c1cdf
Bolodya <Vovan1_2@mail.ru>
2017-02-14 14:35:19 +0700

    recursive

--- Life/src/main/java/ru/nsu/fit/g14203/popov/life/view/GamePanel.java
+++ Life/src/main/java/ru/nsu/fit/g14203/popov/life/view/GamePanel.java
@@ -8,10 +8,10 @@ import java.awt.image.BufferedImage;
 
 class GamePanel extends JPanel {
 
-    int size = 40;
+    int size = 39;
 
     GamePanel() {
-        new Timer(1, e -> repaint()).start();
+//        new Timer(1, e -> repaint()).start();
     }
 
     @Override
@@ -21,17 +21,17 @@ class GamePanel extends JPanel {
         BufferedImage image = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
         image.getGraphics().drawImage(createImage(getWidth(), getHeight()), 0, 0, null);
 
-        for (int i = 0; i < 10; i++) {
-            for (int k = 0; k < 7; k++) {
-                Coordinate[] coords = Cell.getCoords(i, k, size);
-                MyPainter.drawLine(image, coords[0].x, coords[0].y, coords[1].x, coords[1].y, Color.BLACK);
-                MyPainter.drawLine(image, coords[1].x, coords[1].y, coords[2].x, coords[2].y, Color.BLACK);
-                MyPainter.drawLine(image, coords[2].x, coords[2].y, coords[3].x, coords[3].y, Color.BLACK);
-                MyPainter.drawLine(image, coords[3].x, coords[3].y, coords[4].x, coords[4].y, Color.BLACK);
-                MyPainter.drawLine(image, coords[4].x, coords[4].y, coords[5].x, coords[5].y, Color.BLACK);
-                MyPainter.drawLine(image, coords[5].x, coords[5].y, coords[0].x, coords[0].y, Color.BLACK);
+        for (int i = 0; i < 2; i++) {
+            for (int k = 0; k < 5; k++) {
+                Point[] points = Grid.getCoords(i, k, size);
+                MyPainter.drawLine(image, points[0].x, points[0].y, points[1].x, points[1].y, Color.BLACK);
+                MyPainter.drawLine(image, points[1].x, points[1].y, points[2].x, points[2].y, Color.BLACK);
+                MyPainter.drawLine(image, points[2].x, points[2].y, points[3].x, points[3].y, Color.BLACK);
+                MyPainter.drawLine(image, points[3].x, points[3].y, points[4].x, points[4].y, Color.BLACK);
+                MyPainter.drawLine(image, points[4].x, points[4].y, points[5].x, points[5].y, Color.BLACK);
+                MyPainter.drawLine(image, points[5].x, points[5].y, points[0].x, points[0].y, Color.BLACK);
 
-                Coordinate point = Cell.getPoint(i, k, size);
+                Point point = Grid.getPoint(i, k, size);
                 MyPainter.fillArea(image, point.x, point.y, new Color((int) Math.round(Math.random() * 0xFFFFFF)));
             }
         }
